1cd586ddad6c885efd7010eba5c54c0c72e7581a
agusortola <63934179+agusortola@users.noreply.github.com>
2021-08-09 20:53:24 -0300

    añado navBar con link al home

--- src/DetailedPalette.js
+++ src/DetailedPalette.js
@@ -1,8 +1,7 @@
-import { Box, Stack, Button, Center  } from '@chakra-ui/react';
-
+import { Box, Stack, Button, Center, Text  } from '@chakra-ui/react';
 import { motion } from 'framer-motion'
 import { useContext } from 'react';
-import Modal from './Modal';
+import { NavBar } from './NavBar';
 import { PaletteContext } from './PaletteContext';
 
 
@@ -68,119 +67,30 @@ const DetailedPalette = ({ setColor, setShowModal }) => {
         setShowModal(true)
       }
 
-    //   const palettes = [{
-    //     zero: 'Friday',
-    //     one : '#eccc68',
-    //     two : '#ff7f50',
-    //     three : '#ff6b81',
-    //     four : '#ffa502',
-    //     five : '#ff6348',
-    //     six : '#ff4757',
-    //     seven : '#7bed9f',
-    //     eight : '#70a1ff',
-    //     nine : '#5352ed'
-    //   },{
-    //     zero: 'Lollipop',
-    //     one : '#cd84f1',
-    //     two : '#ffcccc',
-    //     three : '#ff4d4d',
-    //     four : '#c56cf0',
-    //     five : '#ffb8b8',
-    //     six : '#ff3838',
-    //     seven : '#32ff7e',
-    //     eight : '#7efff5',
-    //     nine : '#18dcff'
-    //   },{
-    //     zero: 'Impact',
-    //     one : '#f6b93b',
-    //     two : '#e55039',
-    //     three : '#4a69bd',
-    //     four : '#fa983a',
-    //     five : '#eb2f06',
-    //     six : '#1e3799',
-    //     seven : '#e58e26',
-    //     eight : '#b71540',
-    //     nine : '#0c2461'
-    //   },{
-    //     zero: 'Creamy',
-    //     one : '#f3a683',
-    //     two : '#f7d794',
-    //     three : '#778beb',
-    //     four : '#f19066',
-    //     five : '#f5cd79',
-    //     six : '#546de5',
-    //     seven : '#786fa6',
-    //     eight : '#f8a5c2',
-    //     nine : '#63cdda'
-    //   },{
-    //     zero: 'Aquarium',
-    //     one : '#d9ed92',
-    //     two : '#b5e48c',
-    //     three : '#99d98c',
-    //     four : '#76c893',
-    //     five : '#52b69a',
-    //     six : '#34a0a4',
-    //     seven : '#168aad',
-    //     eight : '#1a759f',
-    //     nine : '#184e77'
-    //   },{
-    //     zero: 'Calm',
-    //     one : '#d8f3dc',
-    //     two : '#b7e4c7',
-    //     three : '#95d5b2',
-    //     four : '#74c69d',
-    //     five : '#52b788',
-    //     six : '#40916c',
-    //     seven : '#2d6a4f',
-    //     eight : '#1b4332',
-    //     nine : '#081c15'
-    //   },{
-    //     zero: 'Cinema',
-    //     one : '#b7094c',
-    //     two : '#a01a58',
-    //     three : '#892b64',
-    //     four : '#723c70',
-    //     five : '#5c4d7d',
-    //     six : '#455e89',
-    //     seven : '#2e6f95',
-    //     eight : '#1780a1',
-    //     nine : '#0091ad'
-    //   },{
-    //     zero: 'Freedom',
-    //     one : '#54478c',
-    //     two : '#2c699a',
-    //     three : '#048ba8',
-    //     four : '#0db39e',
-    //     five : '#16db93',
-    //     six : '#83e377',
-    //     seven : '#b9e769',
-    //     eight : '#f1c453',
-    //     nine : '#f29e4c'
-    //   }
-    // ]
-
     const {selectedPalette} = useContext(PaletteContext)
     const palette = selectedPalette
-    
-    return ( 
-        <motion.div variants={containerVariants} initial="hidden" animate="visible">
-        <Box justifyItems='center'>
+
+    return (
+      <>
+      <NavBar /> 
+        <motion.div variants={containerVariants} initial="hidden" animate="visible" marginTop={501}>
+        <Center justifyItems='center' width='100%' padding='5%'>
           <Stack spacing={0} direction='column'>
             <Stack spacing={0} direction='row'>
               <motion.div  w="100%"
               variants={firstRowVariants}
               >
-                <Button colorScheme={palette.one} bg={palette.one} variant="link" borderRadius={0}  color="white" fontWeight={800} fontSize='x-large' w='100%' p='15vh' onClick={()=> handleClick(palette.one)}> {palette.one} </Button>
+                <Button colorScheme={palette.one} bg={palette.one} variant="link" borderRadius={0}  color="white" fontWeight={800} fontSize='x-large' w='100%' p='10vh' onClick={()=> handleClick(palette.one)}> {palette.one} </Button>
               </motion.div>
               <motion.center  w="100%"
               variants={firstRowVariants}
               >
-                <Button colorScheme={palette.two} bg={palette.two} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='15vh' onClick={()=> handleClick(palette.two)}> {palette.two} </Button>
+                <Button colorScheme={palette.two} bg={palette.two} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='10vh' onClick={()=> handleClick(palette.two)}> {palette.two} </Button>
               </motion.center >
               <motion.center  w="100%"
               variants={firstRowVariants}
               >
-                <Button colorScheme={palette.three} bg={palette.three} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='15vh' onClick={()=> handleClick(palette.three)}> {palette.three} </Button>
+                <Button colorScheme={palette.three} bg={palette.three} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='10vh' onClick={()=> handleClick(palette.three)}> {palette.three} </Button>
               </motion.center >
             </Stack>
             
@@ -188,17 +98,17 @@ const DetailedPalette = ({ setColor, setShowModal }) => {
             <motion.center  w="100%"
               variants={secondRowVariants}
               >
-                <Button colorScheme={palette.four} bg={palette.four} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='15vh' onClick={()=> handleClick(palette.four)}> {palette.four} </Button>
+                <Button colorScheme={palette.four} bg={palette.four} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='10vh' onClick={()=> handleClick(palette.four)}> {palette.four} </Button>
               </motion.center >
               <motion.center  w="100%"
               variants={secondRowVariants}
               >
-                <Button colorScheme={palette.five} bg={palette.five} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='15vh' onClick={()=> handleClick(palette.five)}> {palette.five} </Button>
+                <Button colorScheme={palette.five} bg={palette.five} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='10vh' onClick={()=> handleClick(palette.five)}> {palette.five} </Button>
               </motion.center >
               <motion.center  w="100%"
               variants={secondRowVariants}
               >
-                <Button colorScheme={palette.six} bg={palette.six} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='15vh' onClick={()=> handleClick(palette.six)}> {palette.six} </Button>
+                <Button colorScheme={palette.six} bg={palette.six} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='10vh' onClick={()=> handleClick(palette.six)}> {palette.six} </Button>
               </motion.center >
             </Stack>
     
@@ -206,22 +116,23 @@ const DetailedPalette = ({ setColor, setShowModal }) => {
             <motion.center  w="100%"
               variants={thirdRowVariants}
               >
-                <Button colorScheme={palette.seven} bg={palette.seven} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='15vh' onClick={()=> handleClick(palette.seven)}> {palette.seven} </Button>
+                <Button colorScheme={palette.seven} bg={palette.seven} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='10vh' onClick={()=> handleClick(palette.seven)}> {palette.seven} </Button>
               </motion.center >
               <motion.center  w="100%"
               variants={thirdRowVariants}
               >
-                <Button colorScheme={palette.eight} bg={palette.eight} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='15vh' onClick={()=> handleClick(palette.eight)}> {palette.eight} </Button>
+                <Button colorScheme={palette.eight} bg={palette.eight} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='10vh' onClick={()=> handleClick(palette.eight)}> {palette.eight} </Button>
               </motion.center >
               <motion.center  w="100%"
               variants={thirdRowVariants}
               >
-                <Button colorScheme={palette.nine} bg={palette.nine} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='15vh' onClick={()=> handleClick(palette.nine)}> {palette.nine} </Button>
+                <Button colorScheme={palette.nine} bg={palette.nine} variant="link" borderRadius={0} color="white" fontWeight={800} fontSize='x-large' w='100%' p='10vh' onClick={()=> handleClick(palette.nine)}> {palette.nine} </Button>
               </motion.center >
             </Stack>
           </Stack>
-        </Box>
+        </Center>
         </motion.div>
+        </>
      );
 }
 
